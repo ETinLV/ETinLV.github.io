1920f138c400b5b137387d238aec3ea6a72bbaf7
ETinLV <ericturnernv@gmail.com>
2016-06-20 14:35:52 -0700

    added the rest of the files

---
+++ js/main.js
@@ -0,0 +1,147 @@
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
+
+function main() {
+
+    (function () {
+        'use strict';
+
+        /* ==============================================
+         Testimonial Slider
+         =============================================== */
+
+        $('a.page-scroll').click(function() {
+            wscroll(this)
+        });
+
+        function wscroll(e) {
+            if (location.pathname.replace(/^\//,'') == e.pathname.replace(/^\//,'') && location.hostname == e.hostname) {
+                var target = $(e.hash);
+                target = target.length ? target : $('[name=' + e.hash.slice(1) +']');
+            if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
+            }
+        }
+
+        /*====================================
+         Show Menu on Book
+         ======================================*/
+        $(window).bind('scroll', function() {
+            var navHeight = $(window).height() - 100;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        })
+
+        $(document).ready(function() {
+            $("#skills").owlCarousel({
+
+                navigation : false, // Show next and prev buttons
+                slideSpeed : 300,
+                paginationSpeed : 400,
+                autoHeight : true,
+                itemsCustom : [
+                    [0, 1],
+                    [450, 2],
+                    [600, 2],
+                    [700, 2],
+                    [1000, 4],
+                    [1200, 4],
+                    [1400, 4],
+                    [1600, 4]
+                ],
+            });
+
+            $("#clients").owlCarousel({
+
+                navigation : false, // Show next and prev buttons
+                slideSpeed : 300,
+                paginationSpeed : 400,
+                autoHeight : true,
+                itemsCustom : [
+                    [0, 1],
+                    [450, 2],
+                    [600, 2],
+                    [700, 2],
+                    [1000, 4],
+                    [1200, 5],
+                    [1400, 5],
+                    [1600, 5]
+                ],
+            });
+
+            $("#testimonial").owlCarousel({
+                navigation : false, // Show next and prev buttons
+                slideSpeed : 300,
+                paginationSpeed : 400,
+                singleItem:true
+            });
+
+        });
+
+        /*====================================
+         Portfolio Isotope Filter
+         ======================================*/
+        $(window).load(function() {
+            var $container = $('#lightbox');
+            $container.isotope({
+                filter: '*',
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            $('.cat a').click(function() {
+                $('.cat .active').removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $container.isotope({
+                    filter: selector,
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear',
+                        queue: false
+                    }
+                });
+                return false;
+            });
+
+        });
+
+$("#emailForm").on("submit", function(e) {
+        e.preventDefault();
+        $.ajax({
+            url:"https://formspree.io/ericturnernv@gmail.com",
+            method:"POST",
+            data:{
+                Email: $('#InputEmail').val(),
+                Subject: $('#InputSubject').val(),
+                Message: $('#InputMessage').val(),
+                dataType: 'json'
+            }});
+        $('#InputEmail').val('');
+        $('#InputSubject').val('');
+        $('#InputMessage').val('');
+        wscroll(document.getElementById('home'))
+    });
+
+    }());
+
+
+}
+main();
